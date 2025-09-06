9cd12dbc813049d78e5a3aefdd9389a237d2085a
Alexander Kagan <amkagan@umich.edu>
2025-09-05 20:24:27 -0400

    setup file change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 LONG_DESCRIPTION = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.16'
+VERSION = '0.0.17'
 DESCRIPTION = 'InfluenceDiffusion package'
 
 setup(
